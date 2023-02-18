02c132bc16cbb4e4de079ca19bce70b5dc4aa9b6
Wasey Rahman <wasey123435@gmail.com>
2023-02-18 22:07:45 +0600

    Going to index2

---
+++ JS/Geometry.js
@@ -0,0 +1,33 @@
+
+// Area for Triangle,Rhombus,Pentagon
+
+function AreaCalculation(){
+   
+    let number_oneField=parseFloat(document.getElementById('number_one').value);
+    let number_twoField=parseFloat(document.getElementById('number_two').value);
+   
+    
+    let Calculation=0.5*number_oneField*number_twoField;
+    Calculation=Calculation.toFixed(2);
+   
+    document.getElementById("Calculation").textContent =Calculation;
+}
+
+
+// Area for Rectangle,Parallelogram
+function AreaCalculationTwo(){
+   
+    let number_threeField=parseFloat(document.getElementById('number_three').value);
+    let number_fourField=parseFloat(document.getElementById('number_four').value);
+   
+    
+    let Calculate=number_threeField*number_fourField;
+    Calculate=Calculate.toFixed(2);
+    document.getElementById("Calculate").textContent =Calculate;
+}
+
+
+
+
+
+
